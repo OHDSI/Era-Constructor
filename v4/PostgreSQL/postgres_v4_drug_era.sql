42c9282b2a4bc11e226c53b91e0ce28686505569
Daniel Terentyev <daniil.terentyev@odysseusinc.com>
2020-04-17 11:39:01 +0300

    Formatting adjustment

--- v4/PostgreSQL/postgres_v4_drug_era.sql
+++ v4/PostgreSQL/postgres_v4_drug_era.sql
@@ -14,106 +14,106 @@ TRUNCATE <schema>.drug_era;
  
 WITH cteDrugTarget(drug_exposure_id, person_id, drug_concept_id, drug_type_concept_id, drug_exposure_start_date, days_supply, drug_exposure_end_date, ingredient_concept_id) AS
 (
-	-- Normalize DRUG_EXPOSURE_END_DATE to either the existing drug exposure end date, or add days supply, or add 1 day to the start date
-	SELECT
-		d.drug_exposure_id
-		, d.person_id
-		, c.concept_id
-		, d.drug_type_concept_id
-		, d.drug_exposure_start_date
-		, d.days_supply AS days_supply
-		, COALESCE(
-			NULLIF(drug_exposure_end_date, NULL) ---If drug_exposure_end_date != NULL, return drug_exposure_end_date, otherwise go to next case
-			, NULLIF(drug_exposure_start_date + (INTERVAL '1 day' * days_supply), drug_exposure_start_date) ---If days_supply != NULL or 0, return drug_exposure_start_date + days_supply, otherwise go to next case
-			, drug_exposure_start_date + INTERVAL '1 day' ---Add 1 day to the drug_exposure_start_date since there is no end_date or INTERVAL for the days_supply
-		) AS drug_exposure_end_date
-		, c.concept_id AS ingredient_concept_id
-	FROM <schema>.drug_exposure d
-		JOIN <vocabulary_and_concept_schema>.concept_ancestor ca ON ca.descendant_concept_id = d.drug_concept_id
-		JOIN <vocabulary_and_concept_schema>.concept c ON ca.ancestor_concept_id = c.concept_id
-		WHERE c.vocabulary_id = 8 ---8 selects RxNorm from the vocabulary_id
-		AND c.concept_class = 'Ingredient' ---selects the Ingredients only. There are other concept_classes in RxNorm that we are not interested in.
+    -- Normalize DRUG_EXPOSURE_END_DATE to either the existing drug exposure end date, or add days supply, or add 1 day to the start date
+    SELECT
+        d.drug_exposure_id
+        , d.person_id
+        , c.concept_id
+        , d.drug_type_concept_id
+        , d.drug_exposure_start_date
+        , d.days_supply AS days_supply
+        , COALESCE(
+            NULLIF(drug_exposure_end_date, NULL) ---If drug_exposure_end_date != NULL, return drug_exposure_end_date, otherwise go to next case
+            , NULLIF(drug_exposure_start_date + (INTERVAL '1 day' * days_supply), drug_exposure_start_date) ---If days_supply != NULL or 0, return drug_exposure_start_date + days_supply, otherwise go to next case
+            , drug_exposure_start_date + INTERVAL '1 day' ---Add 1 day to the drug_exposure_start_date since there is no end_date or INTERVAL for the days_supply
+        ) AS drug_exposure_end_date
+        , c.concept_id AS ingredient_concept_id
+    FROM <schema>.drug_exposure d
+        JOIN <vocabulary_and_concept_schema>.concept_ancestor ca ON ca.descendant_concept_id = d.drug_concept_id
+        JOIN <vocabulary_and_concept_schema>.concept c ON ca.ancestor_concept_id = c.concept_id
+        WHERE c.vocabulary_id = 8 ---8 selects RxNorm from the vocabulary_id
+        AND c.concept_class = 'Ingredient' ---selects the Ingredients only. There are other concept_classes in RxNorm that we are not interested in.
  
-	/* Depending on the needs of your data, you can put more filters on to your code. We assign 0 to unmapped drug_concept_id's, and we found data where days_supply was negative.
- 	 * We don't want different drugs put in the same era, so the code below shows how we filtered them out.
-	 * We also don't want negative days_supply, because that will pull our end_date before the start_date due to our second parameter in the COALESCE function.
- 	 * For now, we are filtering those out as well, but this is a data quality issue that we are trying to solve.
- 	 */
-		---AND d.drug_concept_id != 0
-		---AND d.days_supply >= 0
+    /* Depending on the needs of your data, you can put more filters on to your code. We assign 0 to unmapped drug_concept_id's, and we found data where days_supply was negative.
+      * We don't want different drugs put in the same era, so the code below shows how we filtered them out.
+     * We also don't want negative days_supply, because that will pull our end_date before the start_date due to our second parameter in the COALESCE function.
+      * For now, we are filtering those out as well, but this is a data quality issue that we are trying to solve.
+      */
+        ---AND d.drug_concept_id != 0
+        ---AND d.days_supply >= 0
 )
 --------------------------------------------------------------------------------------------------------------
 , cteEndDates (person_id, ingredient_concept_id, end_date, start_ordinal, overall_ord) AS -- the magic
 (
-	SELECT
-		person_id
-		, ingredient_concept_id
-		, event_date - INTERVAL '30 days' AS end_date
-		, start_ordinal
-		, overall_ord -- unpad the end date
-	FROM
-	(
-		SELECT
-			person_id
-			, ingredient_concept_id
-			, event_date
-			, event_type
-			, MAX(start_ordinal) OVER (PARTITION BY person_id, ingredient_concept_id ORDER BY event_date, event_type ROWS unbounded preceding) AS start_ordinal
-			, ROW_NUMBER() OVER (PARTITION BY person_id, ingredient_concept_id -- this pulls the current START down from the prior rows so that the NULLs from the END DATES will contain a value we can compare with
-				ORDER BY event_date, event_type) AS overall_ord -- this re-numbers the inner UNION so all rows are numbered ordered by the event date
-		FROM
-		(
-			-- select the start dates, assigning a row number to each
-			SELECT
-				person_id
-				, ingredient_concept_id
-				, drug_exposure_start_date AS event_date
-				, -1 AS event_type
-				, ROW_NUMBER() OVER (PARTITION BY person_id, drug_concept_id ORDER BY drug_exposure_start_date) AS start_ordinal
-			FROM cteDrugTarget
-		
-			UNION ALL
-		
-			-- pad the end dates by 30 to allow a grace period for overlapping ranges.
-			SELECT
-				person_id
-				, ingredient_concept_id
-				, drug_exposure_end_date + INTERVAL '30 days'
-				, 1 AS event_type
-				, NULL
-			FROM cteDrugTarget
-		) RAWDATA
-	) e
-	WHERE (2 * e.start_ordinal) - e.overall_ord = 0 
+    SELECT
+        person_id
+        , ingredient_concept_id
+        , event_date - INTERVAL '30 days' AS end_date
+        , start_ordinal
+        , overall_ord -- unpad the end date
+    FROM
+    (
+        SELECT
+            person_id
+            , ingredient_concept_id
+            , event_date
+            , event_type
+            , MAX(start_ordinal) OVER (PARTITION BY person_id, ingredient_concept_id ORDER BY event_date, event_type ROWS unbounded preceding) AS start_ordinal
+            , ROW_NUMBER() OVER (PARTITION BY person_id, ingredient_concept_id -- this pulls the current START down from the prior rows so that the NULLs from the END DATES will contain a value we can compare with
+                ORDER BY event_date, event_type) AS overall_ord -- this re-numbers the inner UNION so all rows are numbered ordered by the event date
+        FROM
+        (
+            -- select the start dates, assigning a row number to each
+            SELECT
+                person_id
+                , ingredient_concept_id
+                , drug_exposure_start_date AS event_date
+                , -1 AS event_type
+                , ROW_NUMBER() OVER (PARTITION BY person_id, drug_concept_id ORDER BY drug_exposure_start_date) AS start_ordinal
+            FROM cteDrugTarget
+        
+            UNION ALL
+        
+            -- pad the end dates by 30 to allow a grace period for overlapping ranges.
+            SELECT
+                person_id
+                , ingredient_concept_id
+                , drug_exposure_end_date + INTERVAL '30 days'
+                , 1 AS event_type
+                , NULL
+            FROM cteDrugTarget
+        ) RAWDATA
+    ) e
+    WHERE (2 * e.start_ordinal) - e.overall_ord = 0 
  
 )
 --------------------------------------------------------------------------------------------------------------
 , cteDrugExposureEnds (person_id, drug_concept_id, drug_type_concept_id, drug_exposure_start_date, drug_era_end_date) AS
 (
 SELECT 
-	dt.person_id
-	, dt.ingredient_concept_id
-	, dt.drug_type_concept_id
-	, dt.drug_exposure_start_date
-	, MIN(e.end_date) AS era_end_date
+    dt.person_id
+    , dt.ingredient_concept_id
+    , dt.drug_type_concept_id
+    , dt.drug_exposure_start_date
+    , MIN(e.end_date) AS era_end_date
 FROM cteDrugTarget dt
 JOIN cteEndDates e ON dt.person_id = e.person_id AND dt.ingredient_concept_id = e.ingredient_concept_id AND e.end_date >= dt.drug_exposure_start_date
 GROUP BY 
-      	 dt.drug_exposure_id
-      	 , dt.person_id
-	 , dt.ingredient_concept_id
-	 , dt.drug_type_concept_id
-	 , dt.drug_exposure_start_date
+           dt.drug_exposure_id
+           , dt.person_id
+     , dt.ingredient_concept_id
+     , dt.drug_type_concept_id
+     , dt.drug_exposure_start_date
 )
 --------------------------------------------------------------------------------------------------------------
 INSERT INTO <schema>.drug_era(person_id, drug_concept_id, drug_era_start_date, drug_era_end_date, drug_type_concept_id, drug_exposure_count)
 SELECT
-	person_id
-	, drug_concept_id
-	, MIN(drug_exposure_start_date) AS drug_era_start_date
-	, drug_era_end_date
-	, drug_type_concept_id
-	, COUNT(*) AS drug_exposure_count
+    person_id
+    , drug_concept_id
+    , MIN(drug_exposure_start_date) AS drug_era_start_date
+    , drug_era_end_date
+    , drug_type_concept_id
+    , COUNT(*) AS drug_exposure_count
 FROM cteDrugExposureEnds
 GROUP BY person_id, drug_concept_id, drug_type_concept_id, drug_era_end_date
 ORDER BY person_id, drug_concept_id
--- v5/PostgreSQL/postgres_v5_condition_era.sql
+++ v5/PostgreSQL/postgres_v5_condition_era.sql
@@ -19,7 +19,7 @@ CREATE TEMPORARY TABLE <schema>.tmp_condition_target AS
         co.person_id,
         co.condition_concept_id,
         co.condition_start_date,
-        COALESCE(co.condition_end_date, condition_start_date + INTERVAL '1 day') AS condition_end_date
+        COALESCE(co.condition_end_date, condition_start_date + INTERVAL '1 day')    AS condition_end_date
     FROM
         <schema>.condition_occurrence co
     /* Depending on the needs of your data, you can put more filters on to your code. We assign 0 to our unmapped condition_concept_id's,
@@ -32,17 +32,29 @@ DROP TABLE IF EXISTS <schema>.tmp_condition_era_s1;
 ---- Step one. Assign 0 or 1 for each period if it's overlapped by one of previous
 CREATE TEMPORARY TABLE <schema>.tmp_condition_era_s1 AS
     SELECT
-        person_id,
-        condition_concept_id,
-        condition_start_date,
-        condition_end_date,
-        condition_occurrence_id,                                            
+        person_id                                                                   AS person_id,
+        condition_concept_id                                                        AS condition_concept_id,
+        condition_start_date                                                        AS condition_start_date,
+        condition_end_date                                                          AS condition_end_date,
+        condition_occurrence_id                                                     AS condition_occurrence_id,                                            
         CASE
             WHEN
-                DATEDIFF(MAX(condition_end_date) OVER (PARTITION BY person_id, condition_concept_id ORDER BY condition_start_date, condition_end_date ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING ), condition_start_date) <= 30 ---- Here you can set number of allowed days between periods
-                THEN 0
+                DATEDIFF(
+                    MAX(condition_end_date) OVER (
+                        PARTITION BY
+                            person_id,
+                            condition_concept_id
+                        ORDER BY
+                            condition_start_date,
+                            condition_end_date
+                        ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING
+                    ),
+                    condition_start_date
+                ) <= 30 ---- Here you can set number of allowed days between periods
+            THEN 0
+
             ELSE 1
-        END AS np_flag
+        END                                                                         AS np_flag
     FROM
         <schema>.tmp_condition_target
 ;
@@ -51,22 +63,35 @@ DROP TABLE IF EXISTS <schema>.tmp_condition_era_s2;
 ---- Step two. Enumerate periods by summing flags
 CREATE TEMPORARY TABLE <schema>.tmp_condition_era_s2 AS
     SELECT
-        person_id,
-        condition_concept_id,
-        condition_start_date,
-        condition_end_date,
-        condition_occurrence_id,
-        SUM(np_flag) OVER (PARTITION BY person_id, condition_concept_id ORDER BY condition_start_date, condition_end_date) AS period_id
+        person_id                               AS person_id,
+        condition_concept_id                    AS condition_concept_id,
+        condition_start_date                    AS condition_start_date,
+        condition_end_date                      AS condition_end_date,
+        condition_occurrence_id                 AS condition_occurrence_id,
+        SUM(np_flag) OVER (
+            PARTITION BY
+                person_id,
+                condition_concept_id
+            ORDER BY
+                condition_start_date,
+                condition_end_date
+        )                                       AS period_id
     FROM
         <schema>.tmp_condition_era_s1
 ;
 
 TRUNCATE <schema>.condition_era;
 ---- Step three. Insert data into condition_era table
-INSERT INTO <schema>.condition_era(person_id, condition_concept_id, condition_era_start_date, condition_era_end_date, condition_occurrence_count)
-SELECT
+INSERT INTO <schema>.condition_era(
     person_id,
     condition_concept_id,
+    condition_era_start_date,
+    condition_era_end_date,
+    condition_occurrence_count
+)
+SELECT
+    person_id                   AS person_id,
+    condition_concept_id        AS condition_concept_id,
     MIN(condition_start_date)   AS condition_era_start_date,
     MAX(condition_end_date)     AS condition_era_end_date,
     COUNT(*)                    AS condition_occurrence_count
